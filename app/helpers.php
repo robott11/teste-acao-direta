cee25316d3e9cab8017f853141c6cb072bb26e09
robott11 <contato.robott@gmail.com>
2022-10-08 16:05:08 -0300

    updated dd helper function

--- app/helpers.php
+++ app/helpers.php
@@ -4,7 +4,7 @@ if (!function_exists('dd')) {
     function dd(mixed ...$vars) {
         foreach ($vars as $var) {
             echo '<pre>';
-            var_dump($var);
+            print_r($var);
             echo '</pre>';
         }
 
