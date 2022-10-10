f90d2404b84a164d45bdf387eeea236698571f1f
robott11 <contato.robott@gmail.com>
2022-10-10 01:28:58 -0300

    points table

--- database/tables.sql
+++ database/tables.sql
@@ -4,3 +4,10 @@ CREATE TABLE users (
     name varchar(255),
     password varchar(61) NOT NULL
 );
+
+CREATE TABLE points (
+    id bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id bigint(20) NOT NULL,
+    is_entrance boolean NOT NULL DEFAULT true,
+    hour datetime NOT NULL DEFAULT now()
+);
