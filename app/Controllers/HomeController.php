3542f02241e70f0df3af4336adbe2617301d817f
robott11 <contato.robott@gmail.com>
2022-10-09 22:09:27 -0300

    home sidebar

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,13 +4,15 @@ namespace App\Controllers;
 
 use App\Http\Request;
 use App\Http\Response;
+use App\Kernel\Auth;
 
 class HomeController extends Controller
 {
     public function index(): Response
     {
         $data = [
-            'title' => 'Home page'
+            'title' => 'Home page',
+            'user' => Auth::getAuthUser()
         ];
 
         return $this->view('home/index', 'layouts/master', $data);
--- views/home/index.php
+++ views/home/index.php
@@ -1,19 +1,32 @@
 <div class="row">
-    <div class="col-2">
-        <ul class="nav nav-pills flex-column">
+    <div class="col-2 bg-dark">
+        <ul class="nav nav-pills flex-column p-3 vh-100 text-light">
             <li class="nav-item">
                 <h2 class="p-3 d-flex justify-content-center">Empresa XYZ</h2>
+                <hr>
             </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Active</a>
+            <li class="nav-item text-light">
+                <div class="dropdown">
+                    <a class="btn dropdown-toggle w-100 text-light border-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <?php echo $user->username ?>
+                    </a>
+
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item text-danger" href="/logout">Sair</a></li>
+                    </ul>
+                </div>
+                <button class="btn btn-outline-secondary text-light rounded-pill w-100 mb-4">REGISTRAR PONTO</button>
             </li>
-            <li class="nav-item">
+            <li class="nav-item mb-2">
+                <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+            </li>
+            <li class="nav-item mb-2"">
                 <a class="nav-link" href="#">Link</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item mb-2"">
                 <a class="nav-link" href="#">Link</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item mb-2"">
                 <a class="nav-link" href="#">Disabled</a>
             </li>
         </ul>
