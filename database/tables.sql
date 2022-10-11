f1b6e6392bce020c9179333f14c2fe0d11d905c8
robott11 <contato.robott@gmail.com>
2022-10-10 22:50:47 -0300

    added admins table

--- database/tables.sql
+++ database/tables.sql
@@ -1,7 +1,14 @@
 CREATE TABLE users (
     id bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username varchar(255) NOT NULL,
-    name varchar(255),
+    name varchar(255) NOT NULL,
+    password varchar(61) NOT NULL
+);
+
+CREATE TABLE admins (
+    id bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username varchar(255) NOT NULL,
+    name varchar(255) NOT NULL,
     password varchar(61) NOT NULL
 );
 
